e2d4f09969f00cce3e093fa5e5efd41bf505d965
RicsiToth <richard99.toth@gmail.com>
2021-12-03 23:38:19 +0100

    Improved header

--- Cinkaren/client/src/App.js
+++ Cinkaren/client/src/App.js
@@ -20,11 +20,12 @@ function App() {
         <Routes>
           <Route path="" element={<p>TODO MAIN PAGE</p>}/>
           <Route path="/signin" element={<p>TODO SIGN IN</p>}/>
+          <Route path="/signout" element={<p>TODO SIGN OUT</p>}/>
           <Route path="/signup" element={<p>TODO SIGN UP</p>}/>
           <Route path="/search" element={<p>TODO SEARCH</p>}/>
-          <Route path="/treners" element={<p>NOT IN THE SCOPE OF ROUND 2</p>}/>
-          <Route path="/profil" element={<p>NOT IN THE SCOPE OF ROUND 2</p>}/>
-          <Route path="/kalendar" element={<Calendar/>}/>
+          <Route path="/trainers" element={<p>NOT IN THE SCOPE OF ROUND 2</p>}/>
+          <Route path="/profile" element={<p>NOT IN THE SCOPE OF ROUND 2</p>}/>
+          <Route path="/mycalendar" element={<Calendar/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- Cinkaren/client/src/Header.js
+++ Cinkaren/client/src/Header.js
@@ -6,29 +6,45 @@ class Header extends React.Component {
     
     constructor(props) {
         super(props);
+        this.location = window.location.href;
+        this.loggedIn = false;
+    }
+
+    logInOutButton(){
+        if(this.loggedIn) {
+            return (
+                <a href="/signout">
+                    <button className="login_logout">Odhlásiť</button>
+                </a>
+            );
+        } else {
+            return (
+                <a href="/signin">
+                    <button className="login_logout">Prihlásiť</button>
+                </a>
+            );
+        }
     }
 
     render() {
       return (
         <header>
             <div class="curved">
-                <div>
+                <a href="/">
                     <h1>Činkáreň</h1>
                     <img src={logo} alt="cinkaren_logo"  width="150" height="100"/>
-                </div>
-                <a href="/signin">
-                    <button className="login_logout">Prihlásiť</button>
                 </a>
+                {this.logInOutButton()}
                 <svg xmlns="http://www.w3.org/2000/svg">
                     <path fill="white" d="M 1600 200 Q 150 150 0 0 L 1600 0 L 1600 600 Q 450 600 300 500 Z"/>
                 </svg>
             </div>
             <nav>
                 <ul>
-                    <li><a class="active" href="/kalendar">Môj kalendár </a></li>
-                    <li><a href="/search">Vyhladať gym </a></li>
-                    <li><a href="/treners">Tréneri </a></li>
-                    <li><a href="/profile">Profil </a></li>
+                    <li><a className={this.location.endsWith("mycalendar") ? "active" : ""} href="/mycalendar">Môj kalendár </a></li>
+                    <li><a className={this.location.endsWith("search") ? "active" : ""} href="/search">Vyhladať gym </a></li>
+                    <li><a className={this.location.endsWith("trainers") ? "active" : ""} href="/trainers">Tréneri </a></li>
+                    <li><a className={this.location.endsWith("profile") ? "active" : ""} href="/profile">Profil </a></li>
                 </ul>
             </nav>
         </header>
