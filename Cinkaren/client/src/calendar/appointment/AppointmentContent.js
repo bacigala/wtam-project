331606187d3834de8c5648885bfdbeed2bb96ff3
Martin Stolarik <60620588+bacigala@users.noreply.github.com>
2022-01-08 18:05:38 +0100

    updated css for events in calendar version A

--- Cinkaren/client/src/calendar/appointment/AppointmentContent.js
+++ Cinkaren/client/src/calendar/appointment/AppointmentContent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {getTimeFromDateString, getHoursFromDateString} from "../../UtilityFunctions";
 import {Appointments} from '@devexpress/dx-react-scheduler-material-ui';
+import moment from 'moment';
 
 class AppointmentContent extends React.Component {
     
@@ -8,13 +9,25 @@ class AppointmentContent extends React.Component {
         super(props);
     }
 
+    getClassNames = (e) => {
+      let classNames = "appointment";
+      let startTime = moment(e.props.data.startDate);
+      let endTime = moment(e.props.data.endDate);
+      var duration = moment.duration(endTime.diff(startTime));
+      if (duration.asHours() < 1) {
+        classNames += " shortAppointment";
+      }
+      return classNames;
+    };
+
     render(){
         return (
           <Appointments.AppointmentContent {...this.props}>
-            <div className="appointment" id={getHoursFromDateString(this.props.data.startDate)}>
+            <div className={this.getClassNames(this)} id={getHoursFromDateString(this.props.data.startDate)}>
               <p className="appointment_name">{this.props.data.name}</p>
-              <p className="appointment_time_from">Od: {getTimeFromDateString(this.props.data.startDate)}</p>
-              <p className="appointment_time_to">Do: {getTimeFromDateString(this.props.data.endDate)}</p>
+              <p className="appointment_time_from">
+                {getTimeFromDateString(this.props.data.startDate)} - {getTimeFromDateString(this.props.data.endDate)}
+              </p>
               <p className="appointment_trainer">{this.props.data.trainer_name + " " + this.props.data.trainer_surname}</p>
             </div>
           </Appointments.AppointmentContent>
